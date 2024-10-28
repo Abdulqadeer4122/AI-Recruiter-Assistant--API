f62a72ef205399befd66ed54db2a4eeb91170b6d
Abdulqadeer4122 <abdul.qadeer4122@gmail.com>
2024-10-28 19:28:15 +0500

    add cors

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from app.routes.route import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from any origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 # Include the routes
 app.include_router(router)
